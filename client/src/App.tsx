71660514a57c8322242963d11dbdc12b0d1f231e
suchetamahata <nosuchetamahatano@gmail.com>
2022-01-27 17:28:10 +0530

    created initial routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Profile from './pages/Profile'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <div className="App">
-      <></>
+      <>
+      <Router>
+        <Switch>
+          <Route path='/profile'>
+            <Profile/>
+          </Route>
+          <Route path='/dashboard'>
+            <Dashboard/>
+          </Route>
+          <Route path='/'>
+          </Route>
+        </Switch>
+      </Router>
+      </>
     </div>
   );
 }
