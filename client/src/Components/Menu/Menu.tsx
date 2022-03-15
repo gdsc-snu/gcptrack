d5675530f768570d79cad37501be18ee047a7d81
maaz619 <maazahmedlfc@gmail.com>
2022-03-15 11:37:38 +0530

    fix: layouts and centered profile page components &added raleway font

--- client/src/Components/Menu/Menu.tsx
+++ client/src/Components/Menu/Menu.tsx
@@ -1,18 +1,17 @@
-import './Menu.css'
-import leaderboardLogo from '../../pictures/LeaderboardLogo.svg'
-import leaderboardOpen from '../../pictures/leaderboardOpen.svg'
-import AdminInfo from '../../pictures/AdminInfo.svg'
-import ClubInfo from '../../pictures/ClubInfo.svg'
-
+import "./Menu.css";
+import leaderboardLogo from "../../pictures/LeaderboardLogo.svg";
+// import leaderboardOpen from '../../pictures/leaderboardOpen.svg'
+import AdminInfo from "../../pictures/AdminInfo.svg";
+import ClubInfo from "../../pictures/ClubInfo.svg";
 
 const Menu = () => {
-    return (
-        <div className='menuDiv'>
-            <img src={leaderboardLogo} className='menuImage top' alt='leaderboard' />
-            <img src={AdminInfo} className='menuImage' alt='adminInfo' />
-            <img src={ClubInfo} className='menuImage' alt='clubInfo' />
-        </div>
-    )
-}
+  return (
+    <div className="menuDiv">
+      <img src={leaderboardLogo} className="menuImage top" alt="leaderboard" />
+      <img src={AdminInfo} className="menuImage" alt="adminInfo" />
+      <img src={ClubInfo} className="menuImage" alt="clubInfo" />
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
--- client/src/Components/ProfileTrack/TrackData.tsx
+++ client/src/Components/ProfileTrack/TrackData.tsx
@@ -1,54 +1,48 @@
-import React from 'react';
-import './TrackData.css';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import { LabDetails } from '../../data/labdetails';
-import ProfileTrack from './ProfileTrack';
+import React from "react";
+import "./TrackData.css";
+import { CircularProgressbar } from "react-circular-progressbar";
+import { LabDetails } from "../../data/labdetails";
+import ProfileTrack from "./ProfileTrack";
 const percentage = 90;
 
 const TrackData = () => {
   return (
     <>
-      <div>
-        <div className='labdetailcss'>
-          <div className='scrolllab'>
-            <div className='main_Box'>
-              <p className='trackName'>Track1</p>
+      <div className="labdetailcss">
+        <div className="scrolllab">
+          <div className="track2">
+            <div className="main_Box">
+              <p className="trackName">Track1</p>
               {LabDetails.map((labdetail) => (
                 <ProfileTrack {...labdetail} />
               ))}
             </div>
-            <div className='circular'>
+            <div className="circular">
               <CircularProgressbar value={percentage} text={`${percentage}%`} />
             </div>
+          </div>
 
-            <div className='track2'>
-              <div className='main_Box'>
-                <p className='trackName'>Track1</p>
-                {LabDetails.map((labdetail) => (
-                  <ProfileTrack {...labdetail} />
-                ))}
-              </div>
-              <div className='circular'>
-                <CircularProgressbar
-                  value={percentage}
-                  text={`${percentage}%`}
-                />
-              </div>
+          <div className="track2">
+            <div className="main_Box">
+              <p className="trackName">Track1</p>
+              {LabDetails.map((labdetail) => (
+                <ProfileTrack {...labdetail} />
+              ))}
             </div>
+            <div className="circular">
+              <CircularProgressbar value={percentage} text={`${percentage}%`} />
+            </div>
+          </div>
 
-            <div className='track2'>
-              <div className='main_Box'>
-                <p className='trackName'>Track1</p>
-                {LabDetails.map((labdetail) => (
-                  <ProfileTrack {...labdetail} />
-                ))}
-              </div>
-              <div className='circular'>
-                <CircularProgressbar
-                  value={percentage}
-                  text={`${percentage}%`}
-                />
-              </div>
+          <div className="track2">
+            <div className="main_Box">
+              <p className="trackName">Track1</p>
+              {LabDetails.map((labdetail) => (
+                <ProfileTrack {...labdetail} />
+              ))}
+            </div>
+            <div className="circular">
+              <CircularProgressbar value={percentage} text={`${percentage}%`} />
             </div>
           </div>
         </div>
