abbbfdd17f34b4aadf94545df32a32e37ddb1278
Pritom Rahaman <pritomrahaman1331@gmail.com>
2022-04-10 13:44:42 +0530

    create login as a facilitator button

---
+++ client/src/Components/Modals/Modal.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Modal.css';
+
+function Modal({ closeModal }: any) {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button
+            onClick={() => {
+              closeModal(false);
+            }}
+          >
+            X
+          </button>
+        </div>
+        <div className="title">
+          <h1>Are You Sure You Want to Continue?</h1>
+        </div>
+        <div className="body">
+          <p>Login as a Facilitator</p>
+        </div>
+        <div className="footer">
+          <button
+            onClick={() => {
+              closeModal(false);
+            }}
+            id="cancelBtn"
+          >
+            Cancel
+          </button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
